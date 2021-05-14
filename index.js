49388c90d88cd1b065abaf4301eb9b7801d4e525
CodeKrakken <newholm1@hotmail.com>
2021-05-14 23:30:02 +0100

    Customise for dogecoin - not yet trading profitably

--- index.js
+++ index.js
@@ -9,49 +9,63 @@ const tick = async (config, binanceClient) => {
   // Cancel open orders left from previou tick, if any
   const orders = await binanceClient.fetchOpenOrders(market);
   orders.forEach(async order => {
-    await binanceClient.cancelOrder(order.id, 'BTC/USDT');
+    await binanceClient.cancelOrder(order.id, market);
   });
 
   // Fetch current market prices
   const results = await Promise.all([
-    axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'),
-    axios.get('https://api.coingecko.com/api/v3/simple/price?ids=tether&vs_currencies=usd')
+    // axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'),
+    // axios.get('https://api.coingecko.com/api/v3/simple/price?ids=tether&vs_currencies=usd')
+    axios.get('https://api.binance.com/api/v3/ticker/price?symbol=DOGEBUSD')
   ]);
-  const marketPrice = results[0].data.bitcoin.usd / results[1].data.tether.usd;
+  const marketPrice = results[0].data.price
 
   // Calculate new orders parameters
-  const sellPrice = marketPrice * (1 + spread);
   const buyPrice = marketPrice * (1 - spread);
+  const sellPrice = buyPrice + spread;
   const balances = await binanceClient.fetchBalance();
   const assetBalance = balances.free[asset]; // e.g. 0.01 BTC
   const baseBalance = balances.free[base]; // e.g. 20 USDT
   const sellVolume = assetBalance * allocation;
   const buyVolume = (baseBalance * allocation) / marketPrice;
 
-  console.log(balances.free[base])
   //Send orders
-  await binanceClient.createLimitSellOrder(market, sellVolume, sellPrice);
-  await binanceClient.createLimitBuyOrder(market, buyVolume, buyPrice);
+  console.log(`New tick for ${market}...`)
 
-  console.log(`
-    New tick for ${market}...
-    Created limit sell order for ${sellVolume}@${sellPrice}  
-    Created limit buy order for ${buyVolume}@${buyPrice}  
-  `);
+  if (buyVolume > 1) { 
+    console.log(market)
+    console.log(buyVolume)
+    console.log(buyPrice)
+    // console.log(binanceClient.createLimitBuyOrder(market, buyVolume, buyPrice))
+    await binanceClient.createLimitBuyOrder(market, buyVolume, buyPrice); 
+    console.log(`Created limit buy order for ${buyVolume} DOGE @ $${buyPrice})`)
+  }
+
+  if (sellVolume > 1) { 
+    console.log(market)
+    console.log(sellVolume)
+    console.log(sellPrice)
+    // console.log(binanceClient.createLimitSellOrder(market, sellVolume, sellPrice))
+    await binanceClient.createLimitSellOrder(market, sellVolume, sellPrice);
+    console.log(`Created limit sell order for ${sellVolume} DOGE @ $${sellPrice})`)
+  }
+
 };
 
 const run = () => {
+  
   const config = { 
-    asset: "BTC",
-    base: "USDT",
-    allocation: 0.7,     // Percentage of our available funds that we trade
-    spread: 0.2,         // Percentage above and below market prices for sell and buy orders 
-    tickInterval: 2000  // Duration between each tick, in milliseconds
+    asset: "DOGE",
+    base: "BUSD",
+    allocation: 1,     // Percentage of our available funds that we trade
+    spread: 0.001,         // Percentage above and below market prices for sell and buy orders 
+    tickInterval: 10000  // Duration between each tick, in milliseconds
   };
   const binanceClient = new ccxt.binance({
     apiKey: process.env.API_KEY,
     secret: process.env.API_SECRET
   });
+
   tick(config, binanceClient);
   setInterval(tick, config.tickInterval, config, binanceClient);
 };
