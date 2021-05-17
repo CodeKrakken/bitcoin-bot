ade23c24a2f9bb591e9509cefd1062a1431c7eab
CodeKrakken <newholm1@hotmail.com>
2021-05-17 17:06:17 +0100

    Add second strategy

--- index2.js
+++ index2.js
@@ -1,6 +1,5 @@
-// This strategy buys and sells 90% of each balance. 
-// It will not sell at a loss, instead it waits until it can make a profit.
-// It will not buy during a fall or sell during a rise.
+// This strategy involves buying and selling $15 at a time. Many orders can be open at once.
+// It sets the sell order at the same time as it sets the buy order, therefore it cannot hold during a rise.
 
 require('dotenv').config();
 const ccxt = require('ccxt');
@@ -14,12 +13,12 @@ let state
 function run() {
 
   const config = {
-    asset: "DOGE",
-    base: "USDT",
-    allocation: 0.9,
-    tickInterval: 10000,
+    asset: "BTC",
+    base: "BUSD",
+    allocation: 15,
+    tickInterval: 2000,
     fee: 0.002,
-    minimumTrade: 10
+    margin: 2
   };
   
   const binanceClient = new ccxt.binance({
@@ -36,7 +35,6 @@ async function tick(client, config) {
   const currentPrice = await marketPrice(market)
   const wallet = await getWallet(client, config)
   const orders = await client.fetchOpenOrders(market);
-  cancelBuyOrder(client, market, orders)
   state = getState(orders, wallet, currentPrice)
   report(market, lastPrice, currentPrice, wallet, config)
   trade(market, wallet, currentPrice, client, config)
@@ -47,7 +45,6 @@ function getState(orders, wallet, price) {
   let outcome
   if (orders.length === 1) {
     outcome = 'Selling'
-    console.log(orders)
   } else {
     outcome = wallet.base > wallet.asset * price ? 'Waiting to buy' : 'Waiting to sell'
   }
@@ -61,22 +58,22 @@ function report(market, lastPrice, currentPrice, wallet, config) {
   console.log(`Action: ${state}`)
   console.log(`\nLast Price: ${lastPrice}`)
   console.log(`Current Price: ${currentPrice}`)
-  if (state === 'Waiting to sell') { console.log(`Profit price: ${boughtPrice * (1 + config.fee*2)}`)}
+  if (state === 'Waiting to sell') { console.log(`Profit price: ${boughtPrice * (1 + config.fee*config.margin)}`)}
   if (state === 'Selling') { console.log(`Selling at: ${askingPrice}`)}
   console.log('\n' + comparePrices(lastPrice, currentPrice))
   console.log(`\nWallet\n  ${wallet.base} ${config.base}\n+ ${wallet.asset} ${config.asset}\n= ${wallet.base + wallet.asset * currentPrice} ${config.base}`)
 }
 
-function trade(market, wallet, price, client, config) {
-  if (state === 'Waiting to buy' && rising) {
-    newBuyOrder(market, price, client, config, wallet)
-  } else if (state === 'Waiting to sell' && price > (boughtPrice * (1 + config.fee*3)) && (!rising)) {
+async function trade(market, wallet, price, client, config) {
+  let id = 0
+  if (rising && wallet.base >= config.allocation && wallet.asset >= config.allocation / price) {
+    await newBuyOrder(market, price, client, config, wallet)
     newSellOrder(market, price, client, config, wallet)
   }
 }
 
 async function newBuyOrder(market, price, client, config, wallet) {
-  const assetVolume = wallet.base / price * config.allocation
+  const assetVolume = config.allocation / price
   await client.createLimitBuyOrder(market, assetVolume, price)
   boughtPrice = price
   state = 'Buying'
@@ -84,11 +81,12 @@ async function newBuyOrder(market, price, client, config, wallet) {
 }
 
 async function newSellOrder(market, price, client, config, wallet) {
-  const assetVolume = wallet.asset * config.allocation
-  await client.createLimitSellOrder(market, assetVolume, price)
+  const assetVolume = config.allocation / price
+  const profitPrice = price * (1 + config.fee*config.margin)
+  await client.createLimitSellOrder(market, assetVolume, profitPrice)
   askingPrice = price
   state = 'Selling'
-  console.log(`Created limit sell order for ${assetVolume} ${config.asset} @ $${price}`)
+  console.log(`Created limit sell order for ${assetVolume} ${config.asset} @ $${profitPrice}`)
 }
 
 function comparePrices(lastPrice, currentPrice) {
