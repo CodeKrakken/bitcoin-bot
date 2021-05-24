eab3862e72a550dff2525fd29973bcf17062d326
CodeKrakken <newholm1@hotmail.com>
2021-05-24 14:55:47 +0100

    Display average true range for 100 and 200

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ $(document).ready(function(){
         Last Price : {{ lastPrice }} <br>
         Current Price : {{ this.n(tick.currentPrice, 5) }} <br>
         {{ comparePrices(lastPrice, tick.currentPrice) }} <br>
-        Average True Range : {{ this.n(atr(trimmedHistory)), 5 }}
+        Average True Range, 100 : {{ this.n(this.atr(this.trim(tick.priceHistory), 100), 5) }} <br>
+        Average True Range, 200 : {{ this.n(this.atr(this.trim(tick.priceHistory), 200), 5) }}
       </div>
     `,
     data() {
@@ -45,6 +46,54 @@ $(document).ready(function(){
       },
       n(n, d) {
         return Number.parseFloat(n).toFixed(d);
+      },
+      atr(data, time) {
+        let highs = this.extractData(data, 'high')
+        let lows = this.extractData(data, 'low')
+        let closes = this.extractData(data, 'close')
+        let trueRange  = []
+      
+        for (let i = 1; i - 1 < data.length-1; i++) {
+          let tr1 = (highs[i]-lows[i])
+          let tr2 = Math.abs(highs[i] - closes[i])
+          let tr3 = Math.abs(closes[i-1] - lows[i])
+          trueRange.push(Math.max(tr1, tr2, tr3))
+        }
+      
+        if (time < trueRange.length) {
+          trueRange = trueRange.slice((time * -1))
+        }
+        return this.average(trueRange)
+      },
+      trim(data) {
+        let dataObjectArray = []
+        data.forEach(period => {
+          dataObjectArray.push({
+            'start': period[0],
+            'open': parseFloat(period[1]),
+            'high': parseFloat(period[2]),
+            'low': parseFloat(period[3]),
+            'close': parseFloat(period[4]),
+            'v': period[5],
+            'end': period[6],
+            'qav': period[7],
+            'num_trades': period[8],
+            'taker_base_vol': period[9],
+            'taker_quote_vol': period[10],
+            'ignore': period[11]
+          })
+        })
+        return dataObjectArray
+      },
+      extractData(dataObject, key) {
+        let array = []
+        dataObject.forEach(obj => {
+          array.push(obj[key])
+        })
+        return array
+      },
+      average(array) {
+        return (array.reduce(( a, b ) => a + b, 0 )) / array.length
       }
     }
   })
