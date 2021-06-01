2dddaf88980807648e0bed241e3242057e4527bc
CodeKrakken <newholm1@hotmail.com>
2021-06-01 16:44:33 +0100

    Scope all the style

--- client/src/views/Orders.vue
+++ client/src/views/Orders.vue
@@ -1,7 +1,18 @@
 <template>
 <div>
   <div id="grid">
-
+    <span>1</span>
+    <span>2</span>
+    <span>3</span>
+    <span>4</span>
+    <span>5</span>
+    <span>6</span>
+    <span>7</span>
+    <span>8</span>
+    <span>9</span>
+    <span>10</span>
+    <span>11</span>
+    <span>12</span>
   </div>
 </div>
 </template>
@@ -24,10 +35,15 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 #grid {
-
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 6(1fr);
+  grid-gap: 2px;
+  width: 50%;
+  height: 50%
 }
 
 </style>
